ec61f6d1b13953df86193ba9626e9ac34d50a2e0
User <User@PC>
2015-07-14 16:51:22 +0300

    EndOfDAY

--- Servlet/src/com/jobsukraine/servlet/Servlet.java
+++ Servlet/src/com/jobsukraine/servlet/Servlet.java
@@ -7,18 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Servlet")
+@WebServlet("/Se1rvlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at zero: ").append(request.getContextPath());
+		request.getRequestDispatcher("my.jsp");
+
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
